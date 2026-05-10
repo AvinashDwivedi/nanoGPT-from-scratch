5af606224b2bbc6732771b15e53743949918d94d
AvinashDwivedi <avinashdubeyg2001@gmail.com>
2026-05-10 22:48:54 +0530

    removed batch size bug

--- train.py
+++ train.py
@@ -1,1 +1,32 @@
-im
+import torch
+from model import BigramLanguageModel
+from dataset import get_batch
+from tokenizer import decode, chars
+
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+model = BigramLanguageModel(len(chars))
+model = model.to(device)
+
+optimizer = torch.optim.AdamW(model.parameters(), lr=1e-3)
+
+batch_size = 4
+
+for steps in range(10000):
+    xb, yb = get_batch("train")
+    xb = xb.to(device)
+    yb = yb.to(device)
+
+    logits, loss = model(xb, yb)
+
+    optimizer.zero_grad(set_to_none=True)
+    loss.backward()
+    optimizer.step()
+    if steps % 100 == 0:
+        print(f"step {steps}: loss {loss.item()}")
+
+
+context = torch.zeros((1, 1), dtype=torch.long).to(device)
+
+print(decode(model.generate(context, max_new_tokens=300)[0].tolist()))
